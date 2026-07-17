610bb292b5b9b06f8910f0a4f056948e4d811e67
mariyamkhan-git <malodhi14@gmail.com>
2026-07-17 20:41:56 +0500

    new merge update

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("This is a sample git repo to test some of the git commands!")
 print("This will serve as our conflict.. in the main and test branch")
 print("2+2:",2+2)
 print("new changes underway...")
-
+def stub():
+    print("Merge successful")
 
 print ("we are now going to test a pull request feature along with how to resolve conflicts.")
