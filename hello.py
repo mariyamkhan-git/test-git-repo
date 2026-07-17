3e5da365cb9b9b82ba0a169d3c2774c3b3556cd1
mariyamkhan-git <malodhi14@gmail.com>
2026-07-17 20:24:45 +0500

    testing changes

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("This is a sample git repo to test some of the git commands!")
+print("This will serve as our conflict.. in the main and test branch")
+print("2+2:",2+2)
 print("new changes underway...")
+
+
+print ("we are now going to test a pull request feature along with how to resolve conflicts.")
